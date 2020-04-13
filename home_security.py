e00120b3e2b0271599c1c7fcffb17964f4b1a86e
johnsk95 <topgearsk@gmail.com>
2020-04-12 23:37:40 -0400

    added python code to interact with lidar

--- home_security.py
+++ home_security.py
@@ -1,48 +1,45 @@
-from guizero import App, Text, PushButton, TextBox
-from gpiozero import LED
-from time import sleep
 import time
+import board
+import busio
+import digitalio
+import adafruit_vl53l0x
 from picamera import PiCamera
 
-led = LED(17)
-camera = PiCamera()
+led = digitalio.DigitalInOut(board.D17)
+led.direction = digitalio.Direction.OUTPUT
+
+i2c = busio.I2C(board.SCL, board.SDA)
+lidar = adafruit_vl53l0x.VL53L0X(i2c)
+lidar.measurement_timing_budget = 200000
+
+trigger = True
+alarm = False
 
 timestamp = time.strftime('%b-%d-%Y_%H:%M', time.localtime())
 
-def toggle_alarm():
-	# toggle led and sound, start recording
-	while True:
-		led.on()
-		sleep(0.5)
-		led.off()
-		sleef(0.5)
-		
-def start():
-    start_button.disable()
-    stop_button.enable()
-
-def stop():
-    start_button.enable()
-    stop_button.disable()
-    
-def toggle_camera():
-    camera.start_preview()
-    camera.start_recording(f'/home/pi/pivids/{timestamp}.h264')
-    sleep(5)
-    camera.stop_recording()
-    camera.stop_preview()
-    
-def toggle_mic():
-    return
+def stream_camera():
+	camera.start_preview()
+	camera.start_recording(f'/recordings/{timestamp}.h264')
+	sleep(10)
+	camera.stop_recording()
+	camera.stop_preview
 	
+def play_sound():
+	return
+
+def activate_alarm():
+	print('alarm activated!')
+	for _ in range(10):
+		led.value = True
+		time.sleep(0.5)
+		led.value = False
+		time.sleep(0.5)
 		
-app = App(title='Home Security System')
-appName = Text(app, text='Home Security System', width='fill', align='top')
 
-start_button = PushButton(app, command=start)
-stop_button = PushButton(app, command=stop, text="DISARM", enabled=False)
+while trigger:
+	dist = lidar.range
+	if (dist < 300) and (dist != 0):
+		activate_alarm()
+	time.sleep(0.2)
 
-camera_button = PushButton(app, command=toggle_camera, text='Camera')
-speak_button = PushButton(app, command=toggle_mic, text='Speak')
-
-app.display()
+print('end!')
