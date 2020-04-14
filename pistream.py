4e029d5a9ba9275e70d929811ee6adea4709e6ef
Rachel Tierney <tierne1t@gmail.com>
2020-04-13 23:22:13 -0400

    added cv code

--- pistream.py
+++ pistream.py
@@ -4,7 +4,7 @@ import logging
 import socketserver
 from threading import Condition
 from http import server
-
+import cv2
 PAGE="""\
 <html>
 <head>
@@ -16,6 +16,7 @@ PAGE="""\
 </body>
 </html>
 """
+camera_works = True
 
 class StreamingOutput(object):
     def __init__(self):
@@ -88,3 +89,18 @@ try:
             camera.stop_recording()
 except:
     print("Camera not attached")
+    camera_works = False
+    #stream static video file instead
+    cap = cv2.VideoCapture('vtest.avi')
+    
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    cap.release()
+    cv2.destroyAllWindows()
