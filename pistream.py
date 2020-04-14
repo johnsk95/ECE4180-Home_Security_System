ad699fad9c7386c1d754852e60daede6767c5b12
Rachel Tierney <tierne1t@gmail.com>
2020-04-14 17:18:12 -0400

    commented old io code

--- pistream.py
+++ pistream.py
@@ -72,9 +72,9 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                         with output.condition:
                             output.condition.wait()
                             frame = output.frame
-                            image = cv2.imdecode(frame, cv2.IMREAD_COLOR)
-                            image = cv2.resize(image, (640,480))
-                            out.write(image)
+                            #image = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+                            #image = cv2.resize(image, (640,480))
+                            #out.write(image)
                     else:
                         if(cap.isOpened()):
                             ret, image = cap.read()
