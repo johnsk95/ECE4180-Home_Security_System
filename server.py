ebbb3a2dbff7d9eed974e4c56bf9e4ef7fdc7766
Rachel Tierney <tierne_r@outlook.com>
2020-04-23 22:09:57 -0400

    fixed directory name

--- server.py
+++ server.py
@@ -75,8 +75,8 @@ def recorded_video():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 def get_video_filenames():
-    absFilePath = os.path.abspath(__file__)
-    print(absFilePath+"\\videos")
+    absFilePath = os.path.dirname(__file__)
+    print(absFilePath+"/videos")
     f = []
     path = "videos"
     for (dirpath, dirnames, filenames) in walk(path):
