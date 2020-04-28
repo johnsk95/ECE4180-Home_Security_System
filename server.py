86e03caf1ad6dfdbb1ad4ace1589012f255ede71
Rachel Tierney <tierne_r@outlook.com>
2020-04-28 13:58:29 -0400

    try ogv video

--- server.py
+++ server.py
@@ -17,8 +17,8 @@ cv_lock = threading.Lock()
 cap = cv2.VideoCapture('dolce_faster.mp4')
 app = Flask("app")
 socketio = SocketIO(app)
-fourcc = cv2.VideoWriter_fourcc('F','M','P','4')
-out = cv2.VideoWriter("static/videos/test.mp4", fourcc, 20, (640,480))
+fourcc = cv2.VideoWriter_fourcc('T','H','E','O')
+out = cv2.VideoWriter("static/videos/test.ogv", fourcc, 20, (640,480))
    
 @socketio.on('alarmoff')
 def on_off(msg):
